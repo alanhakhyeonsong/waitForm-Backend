9a612e9fb3e1af7ff2b8ca974419a001ce7565e2
HakHyeon Song <60968342+alanhakhyeonsong@users.noreply.github.com>
2022-06-15 12:47:00 +0900

    Update WaitFormApplication.java

--- src/main/java/me/ramos/WaitForm/WaitFormApplication.java
+++ src/main/java/me/ramos/WaitForm/WaitFormApplication.java
@@ -1,15 +1,10 @@
 package me.ramos.WaitForm;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@RequiredArgsConstructor
 @SpringBootApplication
 public class WaitFormApplication {
-
-	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WaitFormApplication.class, args);
